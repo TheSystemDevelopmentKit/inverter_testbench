9ea0f1813ffd60145d1199c08503ca8afeee31e5
Marko Kosunen <marko.kosunen@aalto.fi>
2022-08-01 16:37:20 +0300

    Testbench for parallel and serial inverters

--- inverter_testbench/__init__.py
+++ inverter_testbench/__init__.py
@@ -1,20 +1,13 @@
 """
-=========
-inverter_testbench
-=========
+==================
+Inverter testbench
+==================
 
-inverter_testbench model template The System Development Kit
-Used as a template for all TheSyDeKick Entities.
+This class demonstrates how you can construct testbeches for your model
+Entities to gain more versatilyty that can be provided by self tests in the 
+mainguard.
 
-Current docstring documentation style is Numpy
-https://numpydoc.readthedocs.io/en/latest/format.html
-
-This text here is to remind you that documentation is important.
-However, youu may find it out the even the documentation of this 
-entity may be outdated and incomplete. Regardless of that, every day 
-and in every way we are getting better and better :).
-
-Initially written by Marko Kosunen, marko.kosunen@aalto.fi, 2017.
+Initially written by Marko Kosunen, marko.kosunen@aalto.fi, 2022.
 
 """
 
@@ -24,6 +17,12 @@ if not (os.path.abspath('../../thesdk') in sys.path):
     sys.path.append(os.path.abspath('../../thesdk'))
 
 from thesdk import *
+
+from inverter import *
+from inverter.controller import controller as inverter_controller
+from inverter.signal_source import signal_source
+from inverter.signal_plotter import signal_plotter
+
 
 import numpy as np
 
@@ -36,10 +35,11 @@ class inverter_testbench(thesdk):
         self.print_log(type='I', msg='Inititalizing %s' %(__name__)) 
         self.proplist = [ 'Rs' ];    # Properties that can be propagated from parent
         self.Rs =  100e6;            # Sampling frequency
-        self.IOS=Bundle()            # Pointer for input data
-        self.IOS.Members['A']=IO()   # Pointer for input data
-        self.IOS.Members['Z']= IO()
+        self.vdd = 1.0               # Suplly voltage
         self.model='py';             # Can be set externally, but is not propagated
+        self.models=['py','sv','vhdl','eldo','spectre']
+        self.configuration='parallel'
+        #self.models=['ngspice']
         self.par= False              # By default, no parallel processing
         self.queue= []               # By default, no parallel processing
 
@@ -53,73 +53,140 @@ class inverter_testbench(thesdk):
     def init(self):
         pass #Currently nohing to add
 
-    def main(self):
-        '''Guideline. Isolate python processing to main method.
-        
-        To isolate the interna processing from IO connection assigments, 
-        The procedure to follow is
-        1) Assign input data from input to local variable
-        2) Do the processing
-        3) Assign local variable to output
+    def parallel(self):
+        ''' Implements a configuration with a signal source, parallel inverters and plotters
 
         '''
-        inval=self.IOS.Members['A'].Data
-        out=inval
-        if self.par:
-            self.queue.put(out)
-        self.IOS.Members['Z'].Data=out
+        length=2**8
+        controller=inverter_controller()
+        controller.Rs=self.Rs
+        #controller.reset()
+        #controller.step_time()
+        controller.start_datafeed()
+
+        # Here we instantiate the signal source
+        duts=[]
+        plotters=[]
+        #Here we construct the 'testbench'
+        s_source=signal_source()
+        for model in self.models:
+            # Create an inverter
+            d=inverter(self) # this is self is the parent
+            duts.append(d) 
+            d.model=model
+            # Enable debug messages
+            #d.DEBUG = True
+            # Run simulations in interactive modes to monitor progress/results
+            #d.interactive_spice=True
+            #d.interactive_rtl=True
+            # Preserve the IO files or simulator files for debugging purposes
+            # d.preserve_iofiles = True
+            # d.preserve_spicefiles = True
+            # Save the entity state after simulation
+            #d.save_state = True
+            #d.save_database = True
+            # Optionally load the state of the most recent simulation
+            #d.load_state = 'latest'
+            # This connects the input to the output of the signal source
+            d.IOS.Members['A']=s_source.IOS.Members['data']
+            # This connects the clock to the output of the signal source
+            d.IOS.Members['CLK']=s_source.IOS.Members['clk']
+            d.IOS.Members['control_write']=controller.IOS.Members['control_write']
+            ## Add plotters
+            p=signal_plotter()
+            plotters.append(p) 
+            p.plotmodel=d.model
+            p.plotvdd=self.vdd
+            p.Rs = self.Rs
+            p.IOS.Members['A']=d.IOS.Members['A']
+            p.IOS.Members['Z']=d.IOS.Members['Z']
+            p.IOS.Members['A_OUT']=d.IOS.Members['A_OUT']
+            p.IOS.Members['A_DIG']=d.IOS.Members['A_DIG']
+            p.IOS.Members['Z_ANA']=d.IOS.Members['Z_ANA']
+            p.IOS.Members['Z_RISE']=d.IOS.Members['Z_RISE']
+        
+
+        # Here we run the instances
+        s_source.run() # Creates the data to the output
+        for d in duts:
+            d.init()
+            d.run()
+        for p in plotters:
+            p.init()
+            p.run()
+
+    def serial(self):
+        """ Configuration for inverter chain according to self.models
+        Plots from intermediate outputs
+
+        """
+        # Construct first
+        controller=inverter_controller()
+        s_source=signal_source()
+        duts=[]
+        plotters=[]
+        for k in range(len(self.models)):
+            d=inverter(self)
+            duts.append(d)
+            if k==0:
+                d.IOS.Members['A']=s_source.IOS.Members['data']
+            else:
+               d.IOS.Members['A']=duts[k-1].IOS.Members['Z']
+
+            d.model=self.models[k]
+            d.IOS.Members['control_write']=controller.IOS.Members['control_write']
+            # Passing the same clock to all inverters (used in spice simulations)
+            d.IOS.Members['CLK']=s_source.IOS.Members['clk']
+            p=signal_plotter()
+            plotters.append(p) 
+            p.plotmodel=d.model
+            print(p.plotmodel)
+            p.plotvdd=self.vdd
+            p.Rs = self.Rs
+            p.IOS.Members['A']=d.IOS.Members['A']
+            p.IOS.Members['Z']=d.IOS.Members['Z']
+            p.IOS.Members['A_OUT']=d.IOS.Members['A_OUT']
+            p.IOS.Members['A_DIG']=d.IOS.Members['A_DIG']
+            p.IOS.Members['Z_ANA']=d.IOS.Members['Z_ANA']
+            p.IOS.Members['Z_RISE']=d.IOS.Members['Z_RISE']
+
+        s_source.run() # Creates the data to the output
+        controller.start_datafeed()
+        for d in duts:
+            d.init()
+            d.run()
+        for p in plotters:
+            p.init()
+            p.run()
+
+
+    def main(self):
+        """ Chooses the model to construct
+
+        """
+        if self.configuration == 'parallel':
+            self.parallel()
+        elif self.configuration == 'serial':
+            self.serial()
+        else:
+            self.print_log(type='E' , msg='Test %s not implemented' %(self.configuration))
 
     def run(self,*arg):
         '''Guideline: Define model depencies of executions in `run` method.
 
         '''
-        if len(arg)>0:
-            self.par=True      #flag for parallel processing
-            self.queue=arg[0]  #multiprocessing.queue as the first argument
         if self.model=='py':
             self.main()
 
 if __name__=="__main__":
-    import matplotlib.pyplot as plt
     from  inverter_testbench import *
-    from  inverter_testbench.controller import controller as inverter_testbench_controller
     import pdb
-    import math
-    length=1024
-    rs=100e6
-    indata=np.cos(2*math.pi/length*np.arange(length)).reshape(-1,1)
-
-    models=[ 'py']
-    duts=[]
-    for model in models:
-        d=inverter_testbench()
-        duts.append(d) 
-        d.model=model
-        d.Rs=rs
-        d.IOS.Members['A'].Data=indata
-        d.init()
-        d.run()
-
-    for k in range(len(duts)):
-        hfont = {'fontname':'Sans'}
-        figure,axes=plt.subplots(2,1,sharex=True)
-        x = np.arange(length).reshape(-1,1)
-        axes[0].plot(x,indata)
-        axes[0].set_ylim(-1.1, 1.1);
-        axes[0].set_xlim((np.amin(x), np.amax(x)));
-        axes[0].set_ylabel('Input', **hfont,fontsize=18);
-        axes[0].grid(True)
-        axes[1].plot(x, duts[k].IOS.Members['Z'].Data)
-        axes[1].set_ylim(-1.1, 1.1);
-        axes[1].set_xlim((np.amin(x), np.amax(x)));
-        axes[1].set_ylabel('Output', **hfont,fontsize=18);
-        axes[1].set_xlabel('Sample (n)', **hfont,fontsize=18);
-        axes[1].grid(True)
-        titlestr = "Myentity model %s" %(duts[k].model) 
-        plt.suptitle(titlestr,fontsize=20);
-        plt.grid(True);
-        printstr="./inv_%s.eps" %(duts[k].model)
-        plt.show(block=False);
-        figure.savefig(printstr, format='eps', dpi=300);
+    tb=inverter_testbench()
+    tb.models=['py','sv','vhdl','eldo','spectre']
+    #tb.configuration='parallel'
+    tb.configuration='serial'
+    tb.run()
+    #This is here to keep the images visible
+    #For batch execution, you should comment the following line 
     input()
 
